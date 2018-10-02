0acf9e7edc10c0d08655dffe2749933fcda9f5cd
CZ <czavaleta@gbtrealty.com>
2018-10-02 08:38:00 -0500

    index.js - refactored this to take out a lot of the logic into a separate file -> lib/server.js
    Server.js - this is all the logic for the server, now index simply
    starts the process
    
    Workers.js - this file got added to include all the background workers
    that will be processing and executing the checks

--- index.js
+++ index.js
@@ -1,124 +1,27 @@
 /*
- *  Primary file for the API
+ *Primary file for the API
  *
 */
 
 // Dependecies
-var http = require('http')
-var https = require('https')
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-var _data = require('./lib/data');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
+var server = require('./lib/server')
+var workers = require('./lib/workers');
 
-// TEST
+// Declare the app
+var app = {};
 
-
-// define what the server does using the http module
-// The server should respond to all requests with a string
-
-// start the server, and have it listen on port 3000
-
-//  create server obj
-var httpServer = http.createServer(function(req, res) {
-	unifiedServer(req, res);	
-});
-
-// Instatiate the HTTP server
-httpServer.listen( config.httpPort, function() {
-	console.log('The server is listening on port: ' + config.httpPort + '\n' )
-});
-
-var httpsServerOptions = {
-	'key': fs.readFileSync('./https/key.pem'),
-	'cert': fs.readFileSync('./https/cert.pem')
+// Init function
+app.init = function() {
+	// Start server
+	server.init();
+	workers.init();
 };
 
-// Instatiate the https server
-var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
-	unifiedServer(req, res);	
-});
+// Exec
+app.init();
 
-// Start the HTTPS server
-httpsServer.listen( config.httpsPort, function() {
-	console.log('The server is listening on port: ' + config.httpsPort + '\n' )
-});
+// Export the app
 
-// Define a req router
-var router = {
-	'ping' : handlers.ping,
-	'hello': handlers.hello,
-	'users': handlers.users
-}
+module.exports = app;
 
-// All the server logic for both http and https
-var unifiedServer = function(req, res) {
 
-	// get url and parse it
-	// get the path from the url
-	// send a response
-	var parsedUrl = url.parse(req.url, true);
-	var path = parsedUrl.pathname;
-	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
-
-	// get the query string, parse it, as an pbject
-	var queryStringObject = parsedUrl.query;
-
-	// get the http method used
-	var method = req.method.toLowerCase();
-
-	// get the headers as an object
-	var headers = req.headers;
-
-	// get the payload if there is any
-	var decoder = new StringDecoder('utf-8');
-	var buffer = '';
-
-	req.on('data', function(data) {
-		buffer += decoder.write(data);
-	});
-
-	req.on('end', function() {
-		buffer += decoder.end();
-
-		// chose handler this req should go to. If one is not found use the notFound handler
-		var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-		// construct the data object to send to the handler
-		var data = {
-			'trimmedPath' : trimmedPath,
-			'queryStringObject': queryStringObject,
-			'method': method,
-			'headers': headers,
-			'payload': helpers.parseJsonToObject(buffer)
-		}
-
-		// route req to the handler specified in handler
-		chosenHandler(data, function(statusCode, payload) {
-
-			// Use the satus code called back by the handler or default to 200
-			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-			// Use the payload called back by the handler, or default to ''
-			payload = typeof(payload) == 'object' ? payload : {};
-
-			// send string back, convert payload to strg
-			var payloadString = JSON.stringify(payload);
-
-			// set the header content type so that apps know we are dealing with JSON
-			res.setHeader('Content-Type', 'application/json');
-
-			// return result
-			res.writeHead(statusCode);
-			res.end(payloadString);
-
-			// log what path the person was asking for
-			console.log('Returning this response: \n', statusCode, payloadString);
-
-		});
-		
-	});
-
-};
